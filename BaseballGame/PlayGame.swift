4d8e522da025e2dc7a68267e1297ab150502f174
Chanho Lee <chanho1228@gmail.com>
2025-03-18 16:30:20 +0900

    feat: record game tryCount by using History class

--- BaseballGame/PlayGame.swift
+++ BaseballGame/PlayGame.swift
@@ -1,39 +1,46 @@
 //
-//  Game.swift
+//  PlayGame.swift
 //  BaseballGame
 //
 //  Created by Chanho Lee on 3/17/25.
 //
 import Foundation
 
-class Game {
+class PlayGame {
     
-    func start() {
+    func excute() {
+        print("\n>>0부터 9까지 중에 서로 다른 3자리 숫자를 입력하세요")
         var isPlaying = true
-        
+        var trycount = 1
         let answer = makeRandomAnswer()
         while isPlaying {
             let userInput = getUerIntput()
             let strikeAndBallCount = determineStrikeAndBall(answer, userInput)
             switch strikeAndBallCount {
             case(0, 1...3):
-                print("\(strikeAndBallCount.1)볼!!\n")
+                print("\n>>\(strikeAndBallCount.1)볼!!")
+                trycount += 1
             case(1...2, 0):
-                print("\(strikeAndBallCount.0)스트라이크!!\n")
+                print("\n>>\(strikeAndBallCount.0)스트라이크!!")
+                trycount += 1
             case(1...2, 1...2):
-                print("\(strikeAndBallCount.0)스트라이크!! \(strikeAndBallCount.1)볼!!\n")
+                print("\n>>\(strikeAndBallCount.0)스트라이크!! \(strikeAndBallCount.1)볼!!")
+                trycount += 1
             case(3, 0):
-                print("정답입니다.\n")
+                print("\n>>정답입니다.\n>>처음화면으로 돌아갑니다.\n")
+                History.setHistory(trycount)
                 isPlaying = false
-                let select = select()
             case(0,0):
-                print("Nothing\n")
+                print("\n>>Nothing")
+                trycount += 1
             default:
                 isPlaying = true
             }
         }
     }
-    
+}
+
+extension PlayGame {
     func makeRandomAnswer() -> Int {
         var answerArray: [Int] = [0, 1, 2, 3, 4, 5, 6 ,7 ,8 ,9]
         var answer: Float = 0
