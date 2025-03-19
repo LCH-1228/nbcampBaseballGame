1ae87908aeec3a59dcec8aceed6ffb22d322baa1
Chanho Lee <chanho1228@gmail.com>
2025-03-19 14:46:20 +0900

    refactor: IntExtension.swift내  isFit 메서드 상수명 isContainZero를 isStartZero로 변경

--- BaseballGame/IntExtension.swift
+++ BaseballGame/IntExtension.swift
@@ -10,8 +10,8 @@ extension Int {
         let array = String(self).map{$0} //Int를 고차함수 map을 사용하여 array로 변환
         let isPositive = self > 0 ? true : false // Int가 양수인지 판단
         let isLengthThree = array.count == 3 ? true : false //Int가 3자리 수인지 판단
-        let isContainZero = array.first == "0" ? false : true // Int의 시작이 0인지 판단
+        let isStartZero = array.first == "0" ? false : true // Int의 시작이 0인지 판단
         let isDuplicated = (array.count != Set(array).count) ? false : true // Int에 중복되는 숫자가 있는지 판단
-        return isPositive && isLengthThree && isContainZero && isDuplicated
+        return isPositive && isLengthThree && isStartZero && isDuplicated
     }
 }
