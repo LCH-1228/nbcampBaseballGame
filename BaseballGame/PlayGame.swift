c13918d14a055b6dbf35a1b7b4bfb697e101d12a
Chanho Lee <chanho1228@gmail.com>
2025-03-30 10:08:21 +0900

    refactor: determineStrikeAndBall 메서드 개선

--- BaseballGame/PlayGame.swift
+++ BaseballGame/PlayGame.swift
@@ -9,6 +9,7 @@ import Foundation
 class PlayGame {
     
     var historyIntence: History //의존성 주입을 받기 위한 변수선언과 init
+    let setGuesslength = 3
     
     init(historyIntence: History) {
         self.historyIntence = historyIntence
@@ -16,34 +17,32 @@ class PlayGame {
     
     //PlayGame의 실행 메서드
     func excute() {
-        print("\n<게임을 시작합니다.>\n>>숫자를 입력하세요")
+        print("\n<\(setGuesslength)자리 숫자야구 게임을 시작합니다.>\n>>\(setGuesslength) 숫자를 입력하세요")
         var isPlaying = true
         //여기서 do를 지정하면 에러 처리시 while루프를 빠져나가 catch로 넘어가기에 try! 사용
         //try? 사용으로 nil 반환시에 대한 예외처리 필요.
-        let testAnswer = try! makeRandomAnswer()
-        let answer = testAnswer[0] * 100 + testAnswer[1] * 10 + testAnswer[2]
+        let answer = try! makeRandomAnswer()
         print("테스트를 위한 정답 표시 : \(answer)") // 테스트를 위한 정답 표시
-        historyIntence.addHistory(type: .answer, value1: answer)
+//        historyIntence.addHistory(type: .answer, value1: answer)
         while isPlaying {
             do {
-                let testuserInput = try getUerIntput()
-                let userInput = testuserInput[0] * 100 + testuserInput[1] * 10 + testuserInput[2]
-                historyIntence.addHistory(type: .userInput, value1: userInput)
+                let userInput = try getUerIntput()
+//                historyIntence.addHistory(type: .userInput, value1: userInput)
                 let strikeAndBallCount = try determineStrikeAndBall(answer, userInput)
                 switch strikeAndBallCount { //strikeAndBallCount 반환값은 튜플 (strike: Int, ball: Int)
-                case(0, 1...3): //범위연산자로 case 지정
+                case(0, 1...setGuesslength): //범위연산자로 case 지정
                     printMessage(type: .onlyBall, value1: strikeAndBallCount.1)
                     historyIntence.addHistory(type: .onlyBall, value1: strikeAndBallCount.1)
                     historyIntence.addCount()
-                case(1...2, 0):
+                case(1..<setGuesslength, 0):
                     printMessage(type: .onlyStrike, value1: strikeAndBallCount.0)
                     historyIntence.addHistory(type: .onlyStrike, value1: strikeAndBallCount.0)
                     historyIntence.addCount()
-                case(1...2, 1...2):
+                case(1..<setGuesslength, 1..<setGuesslength):
                     printMessage(type: .strikeAndBall, value1: strikeAndBallCount.0, value2: strikeAndBallCount.1)
                     historyIntence.addHistory(type: .strikeAndBall, value1: strikeAndBallCount.0, value2: strikeAndBallCount.1)
                     historyIntence.addCount()
-                case(3, 0):
+                case(setGuesslength, 0):
                     printMessage(type: .threeStike)
                     historyIntence.addHistory(type: .threeStike)
                     historyIntence.setHistory()
@@ -65,7 +64,7 @@ class PlayGame {
                 case .inputNumbersStartedNotMatchRule:
                     print(">>조건에 맞지 않는 입력입니다.\n>>숫자는 0으로 시작할 수 없습니다.\n")
                 case .inputNumbersCountNotMatchRule:
-                    print(">>조건에 맞지 않는 입력입니다.\n>>0부터 9까지 3자리 숫자를 입력하세요\n")
+                    print(">>조건에 맞지 않는 입력입니다.\n>>0부터 9까지 \(setGuesslength)자리 숫자를 입력하세요\n")
                 case .inputNumberDuplicationNotMatchRule:
                     print(">>조건에 맞지 않는 입력입니다.\n>>0부터 9까지 중에 서로 다른 숫자를 입력하세요\n")
                 case .determineStrikeAndBallInputError:
@@ -79,12 +78,12 @@ class PlayGame {
 // 코드 가독성을 위해 extension으로 분리
 extension PlayGame {
     func makeRandomAnswer() throws -> [Int] {
-        var answerArray: [Int] = [0, 1, 2, 3, 4, 5, 6 ,7 ,8 ,9] //숫자 선택을 위한 배열
+        let answerArray: [Int] = [0, 1, 2, 3, 4, 5, 6 ,7 ,8 ,9] //숫자 선택을 위한 배열
         let answer = answerArray.shuffled()
         guard answer[0] != 0 else {
-            return [Int](answer[1...3])
+            return [Int](answer[1...setGuesslength])
         }
-        return [Int](answer[0...2])
+        return [Int](answer[0..<setGuesslength])
     }
     
     //사용자 입력 요청 메서드
@@ -93,25 +92,20 @@ extension PlayGame {
         let numberArray = userInput.map { String($0) }.compactMap { Int($0) }
         guard !numberArray.isEmpty else { throw CustomError.PlayGameError.invaildInput } //사용자 입력이 올바르지 않을때 예외 처리
         guard numberArray[0] != 0 else { throw CustomError.PlayGameError.inputNumbersStartedNotMatchRule } //사용자 입력이 0으로 시작할때 예외 처리
-        guard numberArray.count == 3 else { throw CustomError.PlayGameError.inputNumbersCountNotMatchRule } //사용자 입력 숫자가 3개가 아닐때 예외 처리
+        guard numberArray.count == setGuesslength else { throw CustomError.PlayGameError.inputNumbersCountNotMatchRule } //사용자 입력 숫자가 3개가 아닐때 예외 처리
         guard numberArray.count == Set(numberArray).count else { throw CustomError.PlayGameError.inputNumberDuplicationNotMatchRule } //사용자 입력에 중복된 숫자가 있을때 예외 처리
         return numberArray
     }
     
     //스트라이크 볼 판단 메서드
-    func determineStrikeAndBall(_ answer: Int, _ userInput: Int) throws -> (strike: Int, ball: Int) {
-        if userInput < 0 { //입력값이 음수라면 잘못된 입력으로 예외처리
-            throw CustomError.PlayGameError.determineStrikeAndBallInputError
-        }
+    func determineStrikeAndBall(_ answer: [Int], _ userInput: [Int]) throws -> (strike: Int, ball: Int) {
         var strike = 0
-        let answerArray = String(answer).map {$0}
-        let UserInputArray = String(userInput).map {$0}
-        let answerSet: Set<Character> = Set(answerArray)
-        let UserInputSet: Set<Character> = Set(UserInputArray)
-        var ball = answerSet.intersection(UserInputSet).count
+        let answerSet: Set<Int> = Set(answer)
+        let userInputSet: Set<Int> = Set(userInput)
+        var ball = answerSet.intersection(userInputSet).count
         
-        for i in 0...2 {
-            if answerArray[i] == UserInputArray[i] {
+        for i in 0..<setGuesslength {
+            if answer[i] == userInput[i] {
                 strike += 1
             }
             
